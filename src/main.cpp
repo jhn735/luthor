b388239d634a628be5b0468d37e01f302f549659
John Villa <jhn735@gmail.com>
2015-06-15 20:50:40 -0700

    Tested lexer and token. It works as far as I know.

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,20 @@
 #include <iostream>
-	
+#include "luthor.h"	
 using namespace std;
 int main(int argc, char * argv[]){
 	//parse the args
 	//read the input file
+	Lexer l("test_file.txt");
+	Token t;
+	do{
+		t = l.next_token();
+		if(t.type() == type)
+			cout << "Type: ";
+		else if(t.type() == regexp)
+			cout << "Regexp: ";
+		
+		cout << t.value() << endl;
+	}while(t.type() != token_type::end);
 	//construct the DFA code corresponding to the input file
 	//write the code to file(s)
 return 0;
